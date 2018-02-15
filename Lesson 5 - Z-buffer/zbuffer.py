b92d1ff668b39e23a37992b1f2949026843aee5a
Dennis Aldana <dennis.aldana@interactuamovil.com>
2018-02-15 17:07:53 -0600

    Adds natsuki example

--- Lesson 5 - Z-buffer/zbuffer.py
+++ Lesson 5 - Z-buffer/zbuffer.py
@@ -1,7 +1,8 @@
 from gl import Render
 
 r = Render(800, 600)
-r.load('./miku/HatsuneMiku.obj', (200, 50, 0), (2, 2, 2) )
+# r.load('./miku/HatsuneMiku.obj', (200, 50, 0), (2, 2, 2) )
 # r.load('./cubespherecone.obj', (0, 1, 1), (100, 100, 100) )
+r.load('./natsuki.obj', (0.5, 0, 0), (500, 500, 300) )
 
 r.display()
